ac48ea543c617d591348f10faef8e928bf2215af
NMontesino <namontesino@gmail.com>
2019-12-30 14:04:51 -0500

    Delete and edit icons added

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Delete from '../icons/noun_edit_230514.svg'
+import Edit from '../icons/noun_bin_2956146.svg'
 
 class Task extends Component
 {
@@ -7,8 +9,12 @@ class Task extends Component
     {
         return(
 
-            <div className="px-4 py-2 rounded-full" style={ this.props.active ? {'background-color': this.props.bgHighlight, 'color': this.props.textHighlight, 'font-family': 'Montserrat', 'font-weight': '600'} : {'background-color': this.props.textHighlight, 'color': this.props.bgHighlight, 'font-family': 'Montserrat', 'font-weight': '600'} }>
-                { this.props.task.name }
+            <div className="px-4 py-2 rounded-full flex flex-row align-center justify-between" style={ this.props.active ? {'background-color': this.props.bgHighlight, 'color': this.props.textHighlight, 'font-family': 'Montserrat', 'font-weight': '600'} : {'background-color': this.props.textHighlight, 'color': this.props.bgHighlight, 'font-family': 'Montserrat', 'font-weight': '600'} }>
+                <div>{ this.props.task.name }</div>
+                <div>
+                    <img height="20px" width="20px" src={ Edit }></img>
+                    <img height="20px" width="20px" src={ Delete }></img>
+                </div>
             </div>
     
         )
