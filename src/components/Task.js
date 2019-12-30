a48f9db4140495b8eaba07f1ed17ac65cc5284b9
NMontesino <namontesino@gmail.com>
2019-12-30 14:19:47 -0500

    Delete and edit icons added

--- src/components/Task.js
+++ src/components/Task.js
@@ -12,7 +12,7 @@ class Task extends Component
             <div className="px-4 py-2 rounded-full flex flex-row align-center justify-between" style={ this.props.active ? {'background-color': this.props.bgHighlight, 'color': this.props.textHighlight, 'font-family': 'Montserrat', 'font-weight': '600'} : {'background-color': this.props.textHighlight, 'color': this.props.bgHighlight, 'font-family': 'Montserrat', 'font-weight': '600'} }>
                 <div>{ this.props.task.name }</div>
                 <div className="flex flex-row justify-between" style={{'width': '45px'}}>
-                    <img height="17px" width="17px" src={ Edit }></img>
+                    <img className="mr-4" height="17px" width="17px" src={ Edit }></img>
                     <img height="15px" width="15px" src={ Delete }></img>
                 </div>
             </div>
