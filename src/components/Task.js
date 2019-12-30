f7bcf511ebc8626a23c7c8d8312d3a0771e6c1b3
NMontesino <namontesino@gmail.com>
2019-12-30 14:14:53 -0500

    Delete and edit icons added

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import Delete from '../icons/noun_edit_230514.svg'
-import Edit from '../icons/noun_bin_2956146.svg'
+import Edit from '../icons/noun_edit_230514.svg'
+import Delete from '../icons/noun_bin_2956146.svg'
 
 class Task extends Component
 {
@@ -11,8 +11,8 @@ class Task extends Component
 
             <div className="px-4 py-2 rounded-full flex flex-row align-center justify-between" style={ this.props.active ? {'background-color': this.props.bgHighlight, 'color': this.props.textHighlight, 'font-family': 'Montserrat', 'font-weight': '600'} : {'background-color': this.props.textHighlight, 'color': this.props.bgHighlight, 'font-family': 'Montserrat', 'font-weight': '600'} }>
                 <div>{ this.props.task.name }</div>
-                <div className="flex flex-row justify-between" style={{'width': '25px'}}>
-                    <img height="15px" width="15px" src={ Edit }></img>
+                <div className="flex flex-row justify-between" style={{'width': '45px'}}>
+                    <img height="17px" width="17px" src={ Edit }></img>
                     <img height="15px" width="15px" src={ Delete }></img>
                 </div>
             </div>
